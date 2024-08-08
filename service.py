0271a82a00c3ea89a977dec7833f5fca130aad4a
avagadro <p-avagadro@yandex.com>
2024-08-08 21:28:12 +0300

    Restrict subscription editing for group chats
    Clean code

--- service.py
+++ service.py
@@ -51,22 +51,6 @@ from model import (
 )
 
 
-# def logcommand[S: BugSignalService, T](method: Callable[[S, Update, CCT], Coroutine[Any, Any, T]]):
-#     """ Decorator for command logging """
-#     def _wrapper(self: S, update: Update, context: CCT) -> Coroutine[Any, Any, T]:
-#         # user_id = (update.effective_user or User(-1, 'Unknown', False)).id
-#         # chat_id = (update.effective_chat or Chat(-1, 'PRIVATE')).id
-#         variables = (
-#             method.__name__,
-#             *((user.id, user.name) if (user := update.effective_user) else (None, None)),
-#             chat.id if (chat := update.effective_chat) else None,
-#         )
-#         self.logger.info('Received command `%s` from user=%s [%s] from chat=`%s`', *variables)
-#         result = method(self, update, context)
-#         return result
-#     return _wrapper
-
-
 def checkvars[S: BugSignalService, T, **KW](
         method: Callable[Concatenate[S, Update, CCT, KW], Coroutine[Any, Any, T]]
         ) -> Callable[[S, Update, CCT], Coroutine[Any, Any, T]]:
@@ -102,22 +86,22 @@ def checkvars[S: BugSignalService, T, **KW](
     return _wrapper
 
 
-def allowed_for(roles: UserRole, admin: bool):
+def allowed_for(roles: UserRole, chat_admin: bool):
     """ Decorator for checking permissions
 
     Parameters
     ----------
     roles : UserRole
         A set of roles that are allowed to execute a command
-    admin : bool
+    chat_admin : bool
         Flag for allowing Telegram chat administrators to execute the command
     """
     def _permission_check[S: BugSignalService, T, **KW](method: Callable[Concatenate[S, Update, CCT, KW], Coroutine[Any, Any, T]]):
         async def _empty_handler(self: S, update: Update, context: CCT, *args: KW.args, **kwargs: KW.kwargs) -> T: ...
         async def _wrapper(self: S, update: Update, context: CCT, *args: KW.args, **kwargs: KW.kwargs) -> T:
-            assert (user := update.effective_user) is not None, f'[permission_check] effective_user is None'
-            assert (chat := update.effective_chat) is not None, f'[permission_check] effective_chat is None'
-            assert (message := update.effective_message) is not None, f'[permission_check] effective_message is None'
+            assert (user := update.effective_user) is not None, '[permission_check] effective_user is None'
+            assert (chat := update.effective_chat) is not None, '[permission_check] effective_chat is None'
+            assert (message := update.effective_message) is not None, '[permission_check] effective_message is None'
             # callback answer
             if (callback_query := update.callback_query) is not None:
                 await callback_query.answer(Notification.MESSAGE_QUERY_ANSWER)
@@ -125,11 +109,14 @@ def allowed_for(roles: UserRole, admin: bool):
             user_roles = (set(UserRole(stored_chat.role))
                           if (stored_chat := self.db.chat(user.id)) is not None
                           else set())
-            # check permissions
+            # allowed by UserRole
+            if bool(user_roles.intersection(roles)):
+                return await method(self, update, context, *args, **kwargs)
+            # otherwise check chat permissions
             administrators = ({_admin.user for _admin in await chat.get_administrators()}
                               if chat.type != ChatType.PRIVATE
                               else set())
-            if user_roles.intersection(roles) or (admin and chat.type != ChatType.PRIVATE and user in administrators):
+            if chat_admin and chat.type != ChatType.PRIVATE and user in administrators:
                 return await method(self, update, context, *args, **kwargs)
             # restrict command execution
             self.logger.warning(Notification.LOG_COMMAND_REJECTED % (user.name, user.id))
@@ -165,6 +152,7 @@ class BugSignalService:
         await kwargs['message'].reply_text(Notification.MESSAGE_CHAT_INFORMATION_SAVED)
 
     @checkvars
+    @allowed_for(UserRole.ACTIVE, chat_admin=False)
     async def fox(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Send fox emoji """
         chat_id = self.__get_random_user(context.args, kwargs['user'].id)
@@ -172,7 +160,7 @@ class BugSignalService:
         await context.bot.send_message(chat_id, Emoji.FOX)
 
     @checkvars
-    @allowed_for(UserRole.NECROMANCER, admin=False)
+    @allowed_for(UserRole.NECROMANCER, chat_admin=False)
     async def zombie(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Send zombie emoji """
         chat_id = self.__get_random_user(context.args, kwargs['user'].id)
@@ -180,7 +168,7 @@ class BugSignalService:
         await context.bot.send_message(chat_id, Emoji.ZOMBIE)
 
     @checkvars
-    @allowed_for(UserRole.ACTIVE, admin=False)
+    @allowed_for(UserRole.ACTIVE, chat_admin=False)
     async def check(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Force check all listeners for updates """
         message = await kwargs['message'].reply_text(Notification.MESSAGE_CHECK_LISTENERS)
@@ -193,7 +181,7 @@ class BugSignalService:
         await message.reply_text(Notification.MESSAGE_DONE)
 
     @checkvars
-    @allowed_for(UserRole.ACTIVE, admin=False)
+    @allowed_for(UserRole.ACTIVE, chat_admin=False)
     async def jobstate(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Get current bugSignal state """
         def _jobformat(job: Job[CCT]):
@@ -206,7 +194,7 @@ class BugSignalService:
         await kwargs['message'].reply_text(STATE)
 
     @checkvars
-    @allowed_for(UserRole.MASTER | UserRole.MODERATOR, admin=True)
+    @allowed_for(UserRole.MASTER | UserRole.MODERATOR, chat_admin=False)
     async def actualize(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Actualize listener jobs """
         await self.__actualize(context)
@@ -263,7 +251,7 @@ class BugSignalService:
     # --------------------------------------------------------------------------------
     # Inline main menu
     @checkvars
-    @allowed_for(UserRole.MASTER | UserRole.MODERATOR, admin=True)
+    @allowed_for(UserRole.MASTER | UserRole.MODERATOR | UserRole.DEVELOPER, chat_admin=True)
     async def main_menu(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Show main menu """
         chat_data = kwargs['chat_data']
@@ -298,18 +286,11 @@ class BugSignalService:
                 await context.bot.send_message(kwargs['chat'].id, menupage.title, reply_markup=markup)
             return
         return await self.__menu_commons(update, context)
-        # match inline query callback
-        # content = menupage.content(kwargs['callback_data'])
-        # match content:
-        #     case _:
-        #         return await self.__menu_commons(update, context)
-        # refresh menu
-        # await self.__menu_refresh(update, context)
 
     # --------------------------------------------------------------------------------
     # Inline listeners menu
     @checkvars
-    @allowed_for(UserRole.MASTER | UserRole.MODERATOR, admin=True)
+    @allowed_for(UserRole.MASTER | UserRole.MODERATOR | UserRole.DEVELOPER, chat_admin=False)
     async def listeners_menu(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Handle LISTENERS menu callback """
         if (menupage := kwargs['chat_data'].get('menupage')) is None:
@@ -344,7 +325,7 @@ class BugSignalService:
     # --------------------------------------------------------------------------------
     # Inline chats menu
     @checkvars
-    @allowed_for(UserRole.MASTER | UserRole.MODERATOR, admin=True)
+    @allowed_for(UserRole.MASTER | UserRole.MODERATOR | UserRole.DEVELOPER, chat_admin=False)
     async def chats_menu(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Handle CHATS menu callback """
         if (menupage := kwargs['chat_data'].get('menupage')) is None:
@@ -379,11 +360,18 @@ class BugSignalService:
     # --------------------------------------------------------------------------------
     # Inline subscriptions menu
     @checkvars
-    @allowed_for(UserRole.MASTER | UserRole.MODERATOR, admin=True)
+    @allowed_for(UserRole.MASTER | UserRole.MODERATOR | UserRole.DEVELOPER, chat_admin=True)
     async def subscriptions_menu(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Handle SUBSCRIPTIONS menu callback """
         if (menupage := kwargs['chat_data'].get('menupage')) is None:
             raise MenuError(Notification.ERROR_MENU_PAGE)
+        # restricting subscription editing for group chats
+        if kwargs['chat'].type == ChatType.PRIVATE:
+            available_chats = self.db.chats(active_only=True)
+        elif (_current_chat := self.db.chat(kwargs['chat'].id)) is not None:
+            available_chats = (_current_chat,)
+        else:
+            raise MenuError('Chat is undefined')
         content = menupage.content(kwargs['callback_data'])
         # match inline query callback
         match content:
@@ -392,7 +380,7 @@ class BugSignalService:
                 menupage = InlineMenuPage(
                     pattern=MenuPattern.SUBSCRIPTIONS,
                     title='Choose a chat to manage subscriptions',
-                    items=self.db.chats(active_only=True),
+                    items=available_chats,
                     items_action=Action.LISTENERS,
                     additional_buttons=Button.NAVIGATION | Button.BACK | Button.CLOSE,
                     previous=menupage,
@@ -428,7 +416,7 @@ class BugSignalService:
     # --------------------------------------------------------------------------------
     # Inline roles menu
     @checkvars
-    @allowed_for(UserRole.MASTER, admin=False)
+    @allowed_for(UserRole.MASTER | UserRole.DEVELOPER, chat_admin=False)
     async def roles_menu(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Handle ROLES menu callback """
         if (menupage := kwargs['chat_data'].get('menupage')) is None:
@@ -480,7 +468,7 @@ class BugSignalService:
     # --------------------------------------------------------------------------------
     # shutdown approve menu
     @checkvars
-    @allowed_for(UserRole.MASTER, admin=False)
+    @allowed_for(UserRole.MASTER | UserRole.DEVELOPER, chat_admin=False)
     async def shutdown(self, update: Update, context: CCT, **kwargs: Unpack[ValidatedContext]):
         """ Open shutdown confirmation """
         chat_data = kwargs['chat_data']
@@ -521,11 +509,6 @@ class BugSignalService:
                 # close menu
                 case _:
                     return await self.__menu_commons(update, context)
-        # refresh menu
-        # await self.__menu_refresh(update, context)
-
-
-
 
     # --------------------------------------------------------------------------------
     # --------------------------------------------------------------------------------
